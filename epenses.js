a31227fddf380d19da3a34014abc51e4b66eef68
K-Diane <dkagaju@gmail.com>
2025-02-24 12:01:41 -0600

    first commit

---
+++ epenses.js
@@ -0,0 +1,10 @@
+// starting expenses as an empty array
+
+let expenses = [];
+
+// function that will hold the add expenses , will take the value from the field
+//it will ensure if all the field are filled before adding an axpense
+function addExpense() {
+  const description = document.getElementById("description").value;
+  const amount = document.getElementById("amount").value;
+}
